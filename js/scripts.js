0582b0625a5615091e669dc97d6cf2035fd5c9bd
Amber W <amoothenielson@gmail.com>
2021-11-20 00:09:21 -0800

    Play around with console.log to check if code will return true with numbers that include 3.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //Business Logic
-// function checkForNumbers(number) {
+// function giveMeNumbers(number) {
 //   for (let i = 0; i<= userNumbers; i++) {
 //     const element = i.toString();
 //   }
@@ -11,16 +11,15 @@ $(document).ready(function () {
   $("#userInput").submit(function (event) {
     event.preventDefault();
     let userNumbers = parseInt(document.getElementById("userNumbers").value);
-    console.log(userNumbers)
-//     while (userNumbers > 0) {
-//       if (userNumbers.includes(3)) {
-//         userNumbers.replace("Won't you be my neighbor?");
-//     } else if (userNumbers.includes(2)) {
-//         userNumbers.replace("Beep!");
-//     } else if (userNumbers.includes(1)) {
-//       userNumbers.replace("Boop?");
-//   }
-//   console.log(userNumbers);
-
-});
+    console.log(userNumbers);
+    console.log([userNumbers].includes("3"));
+  });
 })
+ //   while (userNumbers > 0) {
+  //     if (userNumbers.includes(3)) {
+  //       userNumbers.replace("Won't you be my neighbor?");
+  //   } else if (userNumbers.includes(2)) {
+  //       userNumbers.replace("Beep!");
+  //   } else if (userNumbers.includes(1)) {
+  //     userNumbers.replace("Boop?");
+  // }
