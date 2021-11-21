039224af0db2ee0ecfc7649d667edd25cd815a63
Amber W <amoothenielson@gmail.com>
2021-11-20 20:22:07 -0800

    Replace '.replace' with .match to locate 3s within numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,17 @@ $(document).ready(function () {
     let userNumbers = parseInt(document.getElementById("userNumbers").value);
     console.log(userNumbers);
     let stringNum=userNumbers.toString();
-    // (userNumbers.toString().indexOf('3') > -1)
-    while (userNumbers > 0) {
-      if (stringNum.includes(3)) {
-        stringNum.replace("Won't you be my neighbor?");
-      } else if (stringNum.includes(2)) {
-        stringNum.replace("Beep!");
-      } else if (stringNum.includes(1)) {
-        stringNum.replace("Boop?");
-      }
-      }
-    });
+    if(stringNum.match(3)) {
+      alert('Found');
+    }
+  })
+    // while (userNumbers > 0) {
+    //   if (stringNum.includes(3)) {
+    //     stringNum.replace("Won't you be my neighbor?");
+    //   } else if (stringNum.includes(2)) {
+    //     stringNum.replace("Beep!");
+    //   } else if (stringNum.includes(1)) {
+    //     stringNum.replace("Boop?");
+    //   }
+    //   }
   });
