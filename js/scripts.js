a72ffa5ee526072662b69ff63fcf1b4ab493fa9e
Amber W <amoothenielson@gmail.com>
2021-12-01 18:53:47 -0800

    Create loop to hopefully show range of numbers based on inputted number. Fix if else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 //Business Logic
-
+const myArray = []
+for (let i = 0; i < userNumbers; i++) {
+  myArray.push(i)
+}
+let stringNum=userNumbers.toString();
+if(stringNum.match(3)) {
+  myArray.push("Won't you be my neighbor?");
+}
+else if(stringNum.match(2)) {
+  myArray.push("Boop?");
+}
+else if(stringNum.match(1)) {
+  myArray.push("Beep!!!");
+}
+else {
+myArray.push(" " + i); 
+}
 
 //UI Logic
 $(document).ready(function () {
@@ -7,16 +23,6 @@ $(document).ready(function () {
     event.preventDefault();
     let userNumbers = parseInt(document.getElementById("userNumbers").value);
     console.log(userNumbers);
-    let stringNum=userNumbers.toString();
-    if(stringNum.match(3)) {
-      alert("Won't you be my neighbor?");
-    }
-    else if(stringNum.match(2)) {
-      alert('Boop!');
-    }
-    else if(stringNum.match(1)) {
-      alert('Beep!');
-    }
   })
     // while (userNumbers > 0) {
     //   if (stringNum.includes(3)) {
