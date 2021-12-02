5c686ee6a4297031c99835597aef787bfe54e9f9
Amber W <amoothenielson@gmail.com>
2021-12-01 19:46:40 -0800

    simplify code to have if else statement include toString. Make sure loop is reading the array created instead of just the inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,33 @@ function beepBoop(userNumbers){
 
 for (let i = 0; i < userNumbers; i++) {
   myArray.push(i)
-}
-let stringNum=userNumbers.toString();
-if(stringNum.match(3)) {
-  myArray.push("Won't you be my neighbor?");
-}
-else if(stringNum.match(2)) {
-  myArray.push("Boop?");
-}
-else if(stringNum.match(1)) {
-  myArray.push("Beep!!!");
-}
-else {
-myArray.push(" " + i); 
+  if(i.toString().match(3)) {
+    myArray.push("Won't you be my neighbor?");
   }
+  else if(i.toString().match(2)) {
+    myArray.push("Boop?");
+  }
+  else if(i.toString().match(1)) {
+    myArray.push("Beep!!!");
+  }
+  else {
+  myArray.push(" " + i); 
+    }
+  }
+  return myArray
+// let stringNum=myArray.toString();
+// if(stringNum.match(3)) {
+//   myArray.push("Won't you be my neighbor?");
+// }
+// else if(stringNum.match(2)) {
+//   myArray.push("Boop?");
+// }
+// else if(stringNum.match(1)) {
+//   myArray.push("Beep!!!");
+// }
+// else {
+// myArray.push(" " + i); 
+//   }
 }
 //UI Logic
 $(document).ready(function () {
