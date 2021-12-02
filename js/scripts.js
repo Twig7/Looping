0ab13b052aa6960260f9245835f1215c5e883c76
Amber W <amoothenielson@gmail.com>
2021-12-01 19:02:11 -0800

    Attempt to simplify if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 //Business Logic
-const myArray = []
+function beepBoop(userNumbers){
+  const myArray = []
+
 for (let i = 0; i < userNumbers; i++) {
   myArray.push(i)
 }
-let stringNum=userNumbers.toString();
+// let stringNum=userNumbers.toString();
 if(stringNum.match(3)) {
   myArray.push("Won't you be my neighbor?");
 }
@@ -15,15 +17,17 @@ else if(stringNum.match(1)) {
 }
 else {
 myArray.push(" " + i); 
+  }
 }
-
 //UI Logic
 $(document).ready(function () {
   $("#userInput").submit(function (event) {
     event.preventDefault();
     let userNumbers = parseInt(document.getElementById("userNumbers").value);
     console.log(userNumbers);
-  })
+    let output = beepBoop(userNumbers);
+    $("#.result").text(output);
+  });
     // while (userNumbers > 0) {
     //   if (stringNum.includes(3)) {
     //     stringNum.replace("Won't you be my neighbor?");
